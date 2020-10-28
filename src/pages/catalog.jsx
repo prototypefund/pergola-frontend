685b35eee7f35114d9f86275e8f6233330929086
bastiion <graviola@posteo.de>
2020-10-28 12:13:43 +0100

    show GarenMap instead of catalag list on catalog page

--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -1,28 +1,14 @@
-import { List, ListItem,Navbar, Page } from 'framework7-react'
+import {List, Navbar, Page} from 'framework7-react'
 import React from 'react'
 
-export default class extends React.Component {
-  constructor() {
-    super()
+import GardenMap from '../components/GardenMap'
 
-    this.state = {
-      products: this.$f7.data.products,
-    }
-  }
-  render() {
-    return (
-      <Page name="catalog">
-        <Navbar title="Catalog" />
-        <List>
-          {this.state.products.map(( product ) => (
-            <ListItem
-              key={product.id}
-              title={product.title}
-              link={`/product/${product.id}/`}
-            />
-          ))}
-        </List>
-      </Page>
-    )
-  }
+export default function Component() {
+  return (
+    <Page name="catalog">
+      <Navbar title="Catalog"/>
+      <GardenMap/>
+      <List />
+    </Page>
+  )
 }
