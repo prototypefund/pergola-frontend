181734563b298640de9c486ffd4d3fb812a7b526
bastiion <graviola@posteo.de>
2020-12-17 13:21:02 +0100

    adding stories (GardenMap, Pages and PaperDrop)

---
+++ src/stories/LandingPage.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import React from 'react'
+
+import { LandingPage } from '../pages'
+
+export default {
+  title: 'Pergola/LandingPage',
+  component: LandingPage,
+} as Meta
+
+const Template: Story<object> = ( args ) => <LandingPage />
+
+export const LandingPageDefault = Template.bind( {} )
+LandingPageDefault.args = {}
