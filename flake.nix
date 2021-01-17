33fffc0f7a11b3e5d121cd0c34f5dc1b35a09191
Johannes Lötzsch <git@johannesloetzsch.de>
2021-01-17 22:45:06 +0100

    working nix build

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Build the frontend with nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { system="x86_64-linux"; };
+  in
+  rec {
+    legacyPackages.x86_64-linux = {
+      jq = pkgs.jq;
+      node2nix = pkgs.nodePackages.node2nix;
+
+      pergola-frontend-deps = import ./nix/override.nix { inherit pkgs; };
+      pergola-frontend = import ./nix/default.nix { inherit pkgs; };
+    };
+
+    defaultPackage.x86_64-linux = legacyPackages.x86_64-linux.pergola-frontend;
+  };
+}
