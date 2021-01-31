9d30ec65fd598ee7763de8d710fdba6df0b9ff13
Johannes Lötzsch <git@johannesloetzsch.de>
2021-01-31 22:54:51 +0100

    cd: android build + release apk

--- nix/trivial/shell.nix
+++ nix/trivial/shell.nix
@@ -3,9 +3,10 @@ pkgs.mkShell {
   buildInputs = with pkgs; [ nodejs-14_x ];
 
   shellHook = ''
-    [ -d ./node_modules ] && echo -e 'You might want delete ./node_modules first.\n'
+    [ -d node_modules ] && echo -e 'You might want delete ./node_modules first.\n'
 
-    npx yarn && npm rebuild node-sass
+    [ -d node_modules/node-sass ] && npm rebuild node-sass
+    npx yarn
 
     echo -e '\nFor a production build run:'
     echo '> npx yarn run build-prod'
