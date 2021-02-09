98c6f2317dd699b9e5472c18bd07e53633652ede
winzlieb <winzlieb@c3d2.de>
2021-02-09 14:01:09 +0100

    today select button

--- src/pages/LetItRainEntry.tsx
+++ src/pages/LetItRainEntry.tsx
@@ -1,12 +1,13 @@
 import {Box, Button, Container, IconButton, makeStyles, Menu, MenuItem, Paper, Typography} from '@material-ui/core'
-import {Add, MoreVert} from '@material-ui/icons'
+import {Add, MoreVert, Today} from '@material-ui/icons'
 import dayjs from 'dayjs'
 import * as React from 'react'
 import {useRef, useState} from 'react'
-import {useSelector} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {Link, Route, Switch} from 'react-router-dom'
 import {useReactToPrint} from 'react-to-print'
 
+import {selectDay} from '../actions'
 import {PrintableCalendar, WateringHelpDrawer} from '../components'
 import WateringCalendarWeek from '../components/letItRain/WateringCalendarWeek'
 import {WateringDetailDrawer} from '../components/letItRain/WateringDetailDrawer'
@@ -19,6 +20,7 @@ import {LetItRainWizard} from './LetItRainWizard'
 
 
 export function LetItRainEntry() {
+  const dispatch = useDispatch()
   const selectedDay = useSelector<RootState, Date | undefined>(( {letItRain: { selectedDate }} ) => selectedDate )
   const classes = useStyles()
   const [menuAnchor, setMenuAnchor] = useState<HTMLElement | null>( null )
@@ -33,6 +35,9 @@ export function LetItRainEntry() {
           <Box display='flex' justifyContent='space-between' padding='8px'>
             <Typography variant='h2'>Unser Gießplan</Typography>
             <div>
+              <IconButton onClick={() => dispatch( selectDay( new Date())) }>
+                <Today/>
+              </IconButton>
               <IconButton component={Link} to='/watering/wizard/0' >
                 <Add/>
               </IconButton>
