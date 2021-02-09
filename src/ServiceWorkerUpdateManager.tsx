4b84d3048ccaa1bd2dde93182232f42f71008ec0
winzlieb <winzlieb@c3d2.de>
2021-02-09 14:44:07 +0100

    better snackbar in Updatemanager

--- src/ServiceWorkerUpdateManager.tsx
+++ src/ServiceWorkerUpdateManager.tsx
@@ -1,4 +1,4 @@
-import {Button, Snackbar } from '@material-ui/core'
+import {Box, Button, LinearProgress, Snackbar, Typography } from '@material-ui/core'
 import React, {MouseEventHandler,useCallback, useEffect, useRef, useState  } from 'react'
 import { register } from 'register-service-worker'
 
@@ -17,6 +17,7 @@ const useAlive = () => {
 function ServiceWorkerUpdateManager() {
   if( process.env.TARGET !== 'web' ) return null
   const checkAlive = useAlive()
+  const [dismissed, setDismissed] = useState( false )
 
   const [update, setUpdate] = useState<MouseEventHandler<HTMLElement> | undefined>()
   const [refreshing, setRefreshing] = useState( false )
@@ -33,23 +34,54 @@ function ServiceWorkerUpdateManager() {
       },
     } )
   }, [] )
+
+  const [progress, setProgress] = useState( 100 )
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress(( prevProgress ) => {
+        if( prevProgress < 0 ) {
+          setDismissed( true )
+          clearInterval( timer )
+          return 100
+        }
+        return prevProgress - 10 } )
+    }, 500 )
+    return () => {
+      clearInterval( timer )
+    }
+  }, [] )
   return (
-    <>
-      <Snackbar
-        open={!!update}
-        message="New content is available; please refresh."
-        action={
+    <Snackbar
+      open={!!update && !dismissed}
+      onClose={() => setDismissed( true )}
+      message={
+        <Box display="flex" alignItems="center">
+          <Box width="100%" mr={1}>
+            <LinearProgress variant="determinate" value={progress} />
+            <Typography>New version is available - please refresh.</Typography>
+          </Box>
+        </Box>
+      }
+      action={
+        <>
+          <Button
+            variant='outlined'
+            component='button'
+            onClick={() => setDismissed( true )}
+          >
+              later
+          </Button>
           <Button
             variant='outlined'
             component='button'
             onClick={update}
             disabled={refreshing}
           >
-            refresh
+              refresh
           </Button>
-        }
-      />
-    </>
+        </>
+      }
+    />
   )
 }
 
