1b99ad93f5aab199abfb19105c3b3b8745122655
Johannes Lötzsch <git@johannesloetzsch.de>
2021-02-10 18:24:09 +0100

    ColorCodedCalendar

--- src/components/notifications/calendar/ColorCodedCalendar.tsx
+++ src/components/notifications/calendar/ColorCodedCalendar.tsx
@@ -1,38 +1,51 @@
-import 'rsuite/dist/styles/rsuite-default.css'
+import 'rsuite/dist/styles/rsuite-default.css'  // TODO remove this, but keep the default background color and other relevant styles
+import '../../../css/rsuite.scss'
+import './colorCodedCalendar.scss'
 
+import dayjs from 'dayjs'
 import React, { useState } from 'react'
 import { Calendar } from 'rsuite'
 
 import { CalDav, getEventsOfDay } from './CalDav'
 
+function formatTimePeriod(event) {
+  const from = event.startDate.toJSDate()
+  const till = event.endDate?.toJSDate()
+  return dayjs( from ).format( 'HH:mm' )
+         + (till && dayjs(till).isAfter(dayjs(from)) ? dayjs( till ).format( ' - HH:mm' ) : '')
+	 + ' Uhr'
+}
+
 /** a renderer to display availabilities of wateringTasks **/
 function renderCell( date: Date, jcalData = undefined ) {
   const events = getEventsOfDay( date, jcalData )
   return events.map(( event ) => {
-    const assignees = event.component.getFirstPropertyValue(
-      'x-pergola-watering-assigned-count'
-    )
-    const color = { 0: '#FFBD4A', 1: '#F6E6A2' }[assignees] || '#BDE3DC'
+    const color = event.component.getFirstPropertyValue( 'x-pergola-color' ) || 'grey'
     return (
-      <div key={event.uid} style={{ background: color }}>
-        {event.summary}
+      <div className='colorCodedCalendarEntry' key={event.uid}
+        style={{ background: color, width: (Math.floor(100/events.length)).toString()+'%' }}>
+        <p> {event.summary} </p>
       </div>
     )
   } )
 }
 
-export function DayOverview({date, events}) {
+export function DayOverview( {date, events} ) {
   return (
     <div className={'dayOverview'}>
-      <h3> {date.toDateString()} </h3>
+      <h3> {dayjs( date ).format( 'LL' )} </h3>
       <table>
         <tbody>
-          { events.map(event => (
-            <tr key={event.uid}>
-              <td> {event.from} </td>
-              <td> {event.summary} </td>
-            </tr>
-          ))}
+          { events.map( event => {
+            const color = event.component.getFirstPropertyValue( 'x-pergola-color' ) || 'grey'
+
+            return (
+              <tr key={event.uid}>
+                <td style={{background: color}}> &nbsp; </td>
+                <td> {formatTimePeriod(event)} </td>
+                <td> {event.summary} </td>
+              </tr>
+            )} )}
         </tbody>
       </table>
     </div>
@@ -41,13 +54,14 @@ export function DayOverview({date, events}) {
 
 /** A component implementing the Calendar View proposed at https://github.com/community-garden/pergola-fullstack/issues/55 **/
 export function ColorCodedCalendar( {jcalData}:any ) {
-  const [date, setDate] = useState(new Date())
+  const [date, setDate] = useState( new Date())
 
   return (
     <>
-      <Calendar bordered compact isoWeek={true}
-                renderCell={ (date:Date) => renderCell(date, jcalData) }
-		onChange={setDate}  />
+      <Calendar className='rsuite'
+        bordered compact isoWeek={true}
+        renderCell={ ( date:Date ) => renderCell( date, jcalData ) }
+        onChange={setDate}  />
       <DayOverview date={date} events={getEventsOfDay( date, jcalData )} />
     </>
   )
@@ -57,7 +71,7 @@ export function PersonalCalendar() {
   return (
     <>
       <CalDav root={process.env.WEBDAV_URL || 'http://localhost:4001/calendar'}
-              path={'/public/wateringTasks.ics'} >
+        path={'/public/example.ics'} >
         <ColorCodedCalendar />
       </CalDav>
     </>
