bbf9ce45ddba6aa89631aef1baac5c1d19c9f573
Philipp Michael <pminf@posteo.de>
2021-02-15 22:05:03 +0100

    fix: notification input alignment

--- src/components/letItRain/NotificationSettings.tsx
+++ src/components/letItRain/NotificationSettings.tsx
@@ -3,7 +3,7 @@ import {
   Checkbox,
   FormControl,
   FormControlLabel,
-  FormGroup, Input, InputLabel,
+  FormGroup, FormHelperText, Input, InputLabel,
   makeStyles,
   Switch,
   Theme,
@@ -34,16 +34,14 @@ function CheckboxNumberFormControl( {className, labelUnchecked, labelCheckedSing
   }
 
   return (
-    <FormGroup row className={className}>
+    <FormGroup row={true} className={classes.formGroup}>
       {checked && <FormControl>
-        <Input style={{width: '2em' }}  type='number' value={number} onChange={handleNumberChange}/>
+        <Input classes={{ input: classes.numberInput }} type='number' value={number} onChange={handleNumberChange}/>
       </FormControl>}
       <FormControlLabel
-        className={classes.boxedSwitch + ' ' + classes.boxed}
-
         labelPlacement='start'
         label={checked ? ( number > 1 ? labelCheckedPlural : labelCheckedSingular ) : labelUnchecked}
-        control={<Switch id='switch' checked={checked} onChange={event => setChecked( event.target.checked )}/>}
+        control={<Checkbox checked={checked} onChange={event => setChecked( event.target.checked )}/>}
       />
     </FormGroup>
   )
@@ -52,45 +50,57 @@ function CheckboxNumberFormControl( {className, labelUnchecked, labelCheckedSing
 
 export function NotificationSettings() {
   const classes = useStyles()
+  const [mailChecked, setMailChecked] = useState(false)
+  const [pushChecked, setPushChecked] = useState(false)
+  const [rememberChecked, setRememberChecked] = useState(false)
 
   return (
-    <div>
-      <Box className={classes.form} display='flex' flexDirection='column' width='100%'>
-        <FormGroup className={classes.boxed} style={{padding: '8px'}}  row>
+    <Box flexGrow="1">
+      <Typography variant="h3" align="center">Wie sollen wir dich erinnern?</Typography>
+      <Box my={3}>
+        <FormGroup row={true} className={classes.box}>
           <FormControlLabel
-            control={<Checkbox/>}
-            label={'per Email'}
+            labelPlacement='start'
+            label="Per E-Mail"
+            control={<Checkbox onChange={event => setMailChecked( event.target.checked )} />}
           />
           <FormControlLabel
-            control={<Checkbox/>}
-            label={'per Push-Nachricht'}
-
+            labelPlacement='start'
+            label="Per Push-Nachricht"
+            control={<Checkbox onChange={event => setPushChecked( event.target.checked )} />}
           />
         </FormGroup>
-        <CheckboxNumberFormControl
-          labelUnchecked='vor jedem Einsatz' labelCheckedSingular='Tag vor dem Einsatz' labelCheckedPlural='Tage vor dem Einsatz'/>
-        <CheckboxNumberFormControl
-          labelUnchecked='vor jeder Woche' labelCheckedSingular='Tag vor jeder Woche' labelCheckedPlural='Tage vor  jeder Woche'/>
-
+        { ( mailChecked || pushChecked ) &&
+          <Box className={classes.box}>
+            <CheckboxNumberFormControl labelUnchecked='Vor jedem Einsatz' labelCheckedSingular='Tag vor jedem Einsatz' labelCheckedPlural='Tage vor dem Einsatz'/>
+            <CheckboxNumberFormControl labelUnchecked='Vor jeder Woche' labelCheckedSingular='Tag vor jeder Woche' labelCheckedPlural='Tage vor  jeder Woche'/>
+          </Box>
+        }
       </Box>
-    </div>
+      <FormControlLabel
+        label="In Profileinstellungen übernehmen"
+        control={<Checkbox onChange={event => setRememberChecked( event.target.checked )} />}
+      />
+    </Box>
   )
 }
 
 const useStyles = makeStyles(( theme: Theme ) => ( {
-  form: {
-    '& > *': {
-      justifyContent: 'space-between',
-      margin: '8px',
-    }
-  },
-  boxed: {
+  box: {
+    marginBottom: '1rem',
+    padding: '.5rem 1rem',
     backgroundColor: theme.palette.grey['200'],
-    borderRadius: '4px',
-  },
-  boxedSwitch: {
+    borderRadius: theme.shape.borderRadius,
     flexGrow: 1,
-    justifyContent: 'space-between',
-    padding: '8px'
+    textAlign: 'right',
+    justifyContent: 'flex-end'
+  },
+  formGroup: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  numberInput: {
+    width: '2ch',
+    textAlign: 'center',
   }
 } ))
