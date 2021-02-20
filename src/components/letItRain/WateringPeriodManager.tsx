2a21c2ea9518e8681163dbf78f5ec8b9899ee88b
winzlieb <winzlieb@c3d2.de>
2021-02-21 00:50:58 +0100

    Watering period Manager now uses different mutation (planWateringTasks)

--- src/components/letItRain/WateringPeriodManager.tsx
+++ src/components/letItRain/WateringPeriodManager.tsx
@@ -1,54 +1,89 @@
-import {gql, useMutation, useQuery } from '@apollo/client'
-import {IconButton, List, ListItem, ListItemSecondaryAction, ListItemText, makeStyles, Paper, Theme, Typography} from '@material-ui/core'
-import { PlayForWork } from '@material-ui/icons'
+import {gql, useMutation, useQuery} from '@apollo/client'
+import {
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  Input,
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText,
+  makeStyles,
+  Paper,
+  Theme,
+  Typography
+} from '@material-ui/core'
+import {PlayForWork} from '@material-ui/icons'
 import dayjs from 'dayjs'
 import * as React from 'react'
+import {useState} from 'react'
 
 import {fromNeo4jDate, toNeo4jDateInput} from '../../helper'
-import {_Neo4jDateInput, WateringPeriod} from '../../types/graphql'
+import {_Neo4jDateInput, MutationPlanWateringPeriodsArgs, WateringPeriod} from '../../types/graphql'
 
 const WATERING_PERIODS_QUERY = gql`
-query WateringPeriod($date: _Neo4jDateInput) {
-    WateringPeriod(filter: {till_gte: $date}) {
-        _id
-        hasUsersAssigned
-        from { year month day}
-        till { year month day}
+    query WateringPeriod($date: _Neo4jDateInput) {
+        WateringPeriod(filter: {till_gte: $date}) {
+            _id
+            hasUsersAssigned
+            from { year month day}
+            till { year month day}
+        }
     }
-}
 `
 
 const PLAN_WATERING_PERIOD_MUTATION = gql`
-mutation planWateringPeriod($periodId: String!) {
-    planWateringPeriod(periodId: $periodId)
-}
+    mutation planWateringPeriod($planing_ahead: Int!, $periods_predefined: Int!) {
+        planWateringPeriods(planing_ahead: $planing_ahead, periods_predefined: $periods_predefined)
+    }
 `
 
 export function WateringPeriodManager() {
-  const classes = useStyles()
-  const {data: wateringPeriodsData } = useQuery<{WateringPeriod: WateringPeriod[]}, {date: _Neo4jDateInput}>( WATERING_PERIODS_QUERY, {
-    variables: { date: toNeo4jDateInput( new Date()) }
+  const {data: wateringPeriodsData} = useQuery<{ WateringPeriod: WateringPeriod[] }, { date: _Neo4jDateInput }>( WATERING_PERIODS_QUERY, {
+    variables: {date: toNeo4jDateInput( new Date())}
   } )
-  const [planWateringPeriod] = useMutation<any, {periodId: string}>( PLAN_WATERING_PERIOD_MUTATION )
+  const [planWateringPeriods] = useMutation<any, MutationPlanWateringPeriodsArgs>( PLAN_WATERING_PERIOD_MUTATION )
+  const [periodsPredefined, setPeriodsPredefined] = useState( 0 )
 
   return (
     <div>
       <Paper>
-        <List>
-          {wateringPeriodsData?.WateringPeriod.map( waterinPeriod => {
-            const from = dayjs( fromNeo4jDate( waterinPeriod.from ))
-            const till = dayjs( fromNeo4jDate( waterinPeriod.till ))
-            return ( <ListItem key={waterinPeriod._id}>
-              <ListItemText>{from.format( 'LL' )} - {till.format( 'LL' )}</ListItemText>
-              <ListItemSecondaryAction>
-                <IconButton disabled={!!waterinPeriod.hasUsersAssigned} onClick={() => waterinPeriod._id && planWateringPeriod( {variables: {periodId: waterinPeriod._id}} )} ><PlayForWork/></IconButton>
-              </ListItemSecondaryAction>
-            </ListItem> )
-          } )}
-        </List>
+        <Container>
+          <FormGroup>
+            <FormControlLabel label='amount of new periods created after calculation' control={
+              <Input
+                type='number'
+                inputProps={{min: 0, max: 10}}
+                value={periodsPredefined}
+                onChange={e => setPeriodsPredefined( parseInt( e.target.value ) || periodsPredefined )}
+              />}/>
+          </FormGroup>
+        </Container>
+        <Container>
+          <List>
+            {wateringPeriodsData?.WateringPeriod.map( waterinPeriod => {
+              const from = dayjs( fromNeo4jDate( waterinPeriod.from ))
+              const till = dayjs( fromNeo4jDate( waterinPeriod.till ))
+              return ( <ListItem key={waterinPeriod._id}>
+                <ListItemText>{from.format( 'LL' )} - {till.format( 'LL' )}</ListItemText>
+                <ListItemSecondaryAction>
+                  <IconButton disabled={!!waterinPeriod.hasUsersAssigned}
+                    onClick={() => waterinPeriod._id && planWateringPeriods( {
+                      variables: {
+                        planing_ahead: Math.ceil( from.diff( dayjs(), 'day' )),
+                        periods_predefined: periodsPredefined
+                      }
+                    } )}>
+                    <PlayForWork/></IconButton>
+                </ListItemSecondaryAction>
+              </ListItem> )
+            } )}
+          </List>
+        </Container>
       </Paper>
     </div>
   )
 }
 
-const useStyles = makeStyles(( theme: Theme ) => ( {} ))
