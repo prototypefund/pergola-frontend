d731ad022e3f8f3dac4a4128e6bc972e05086bf5
winzlieb <winzlieb@c3d2.de>
2021-02-27 01:54:03 +0100

    more maptiles, watercolor as default

--- src/components/GardenMap.tsx
+++ src/components/GardenMap.tsx
@@ -15,7 +15,7 @@ const WrappedEditableOverlay = withLeaflet( LeafletEditableOverlay )
 
 export function GardenMap() {
   const classes = useStyles()
-  const [zoom] = useState<number>( 18 )
+  const [zoom] = useState<number>( 17 )
   const [position] = useState<L.Leaflet.LatLngExpression>( {
     lat: 51.0833,
     lng: 13.73126,
@@ -60,7 +60,14 @@ export function GardenMap() {
       maxZoom={24}
     >
       <LayersControl position="topright">
-        <LayersControl.BaseLayer checked name="Arcgis Satelite">
+        <LayersControl.BaseLayer checked name="Stamen">
+          <TileLayer
+            attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>'
+            url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png"
+            maxNativeZoom={16}
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Arcgis Satelite">
           <TileLayer
             attribution='&copy; <a href="http://www.esri.com/">Esri</a> i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community '
             url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
@@ -73,7 +80,20 @@ export function GardenMap() {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             maxNativeZoom={18}
           />
-
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Stamen Terrain">
+          <TileLayer
+            attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>'
+            url="https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.png"
+            maxNativeZoom={14}
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Carto Light">
+          <TileLayer
+            attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            maxNativeZoom={18}
+          />
         </LayersControl.BaseLayer>
       </LayersControl>
       <WrappedEditableOverlay
